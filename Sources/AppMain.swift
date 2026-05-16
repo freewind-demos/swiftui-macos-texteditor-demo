31811fcea343fcd4fa2371d2cc6aa1ea22df7aa3
Freewind <nowindlee@gmail.com>
2026-05-16 15:21:03 +0800

    simplify to simplest TextEditor

--- Sources/AppMain.swift
+++ Sources/AppMain.swift
@@ -3,10 +3,9 @@ import SwiftUI
 @main
 struct TextEditorApp: App {
     var body: some Scene {
-        Window("Editor", id: "main") {
+        WindowGroup {
             ContentView()
-                .preferredColorScheme(.light)
         }
-        .defaultSize(width: 860, height: 760)
+        .defaultSize(width: 800, height: 600)
     }
 }
